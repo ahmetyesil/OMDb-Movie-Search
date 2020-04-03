8426bacddc6ee1beaa4da700ca3ce9a8bb9422ca
ahmetyesil <ahmet.koyuyesil@gmail.com>
2020-04-03 12:29:20 +0300

    searchbardaki linkleme sorunu düzeldi.

--- components/search-bar.js
+++ components/search-bar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import store from "../stores"
 import "../styles/components/search-bar.scss"
-
+import Router from 'next/router'
 export default class SearchBar extends React.Component {
 
 
@@ -22,6 +22,7 @@ export default class SearchBar extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
         const {s, type, y} = this.state;
+        Router.push('/');
         store.onSearch(s, type, y)
     }
 
